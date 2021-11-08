f06fb2e813fad4421122d68d1cf88f3e104417ea
Ethan Reinke <ethanreinke2000@gmail.com>
2021-11-08 12:08:54 -0500

    Created additional views for user signup and login Visual UI updates to views

--- WalkIt/WalkIt/Views/ContentView.swift
+++ WalkIt/WalkIt/Views/ContentView.swift
@@ -12,25 +12,30 @@ struct ContentView: View {
     @State private var screenSelection: Int? = nil
     
     var body: some View {
+        
         NavigationView{
             VStack(spacing: 1){
                 NavigationLink(destination: MapScreen(), tag: 1, selection: $screenSelection) {}
                 NavigationLink(destination: SettingsScreen(), tag: 2, selection: $screenSelection) {}
-
-                Text("This page is the menu.")
-                    .padding()
+                Text("WalkIt")
+                    .padding(10)
+                    .font(.system(size: 80))
+                    .foregroundColor(.blue)
+                    .background(Color.orange)
+                    .cornerRadius(15)
+                Spacer()
                 
                 Button(action: {
                         self.screenSelection = 1;
                 }){
-                    Text("Nav to map Screen")
+                    Text("Play     ")
                         .foregroundColor(Color.white)
                         .font(.title2)
                         .padding(10)
                 }
                 .background(Color.blue)
                 .cornerRadius(15)
-                
+                Spacer()
                 Button(action: {
                         self.screenSelection = 2;
                 }){
@@ -41,6 +46,7 @@ struct ContentView: View {
                 }
                 .background(Color.blue)
                 .cornerRadius(15)
+                Spacer()
             }
         }
     }
---
+++ WalkIt/WalkIt/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  WalkIt
+//
+//  Created by user on 2021-11-08.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
