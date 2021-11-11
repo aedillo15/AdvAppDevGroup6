6393ccbf5eaf885ffc1df92439b189f10acc26fc
Ethan Reinke <ethanreinke2000@gmail.com>
2021-11-11 13:23:53 -0500

    Reverted master branch back to older commit Recreating CoreDBHelper and Persistence files

---
+++ WalkIt/WalkIt/Controllers/CoreDBHelper.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDBHelper.swift
+//  WalkIt
+//
+//  Created by user on 2021-11-11.
+//
+
+import Foundation
---
+++ WalkIt/WalkIt/Controllers/Persistence.swift
@@ -0,0 +1,8 @@
+//
+//  Persistence.swift
+//  WalkIt
+//
+//  Created by user on 2021-11-11.
+//
+
+import Foundation
